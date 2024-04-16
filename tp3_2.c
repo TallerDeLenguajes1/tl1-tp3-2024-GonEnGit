da3044d5a323ab9530b933079df8949371e1ccff
GonzaloMartinez <mgonzalo192@gmail.com>
2024-04-16 14:46:05 -0300

    subido .gitignore y punto 2 sin termina

---
+++ tp3_2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define FIL 5
+#define COL 12
+
+int main(int argc, char *argv[])
+{
+    // cada fila corresponde a un año
+    // cada columna un monto, los años no se guardan en la matriz
+}
